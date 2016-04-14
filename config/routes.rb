b62055b5ee7671d429715d3dc9c65976c0b71e9b
Ted <ekeast328@gmail.com>
2016-04-14 19:07:37 -0400

    Checkpoint 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :topics
+
+  resources :topics do
+    resources :bookmarks, except: [:index]
+  end
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
