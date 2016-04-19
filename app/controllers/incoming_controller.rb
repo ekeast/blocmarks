c1bbadd42881bfc3aceb688afb5a76e97df9df79
Ted <ekeast328@gmail.com>
2016-04-19 13:44:43 -0400

    fixing email

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,7 +2,7 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    @user = User.where(email: params[:sender]}
+    @user = User.where(email: "#{params[:sender]}")
     @topic = Topic.find_or_create_by(title: params[:subject], user: @user)
     @bookmark = @topic.bookmarks.new(url: params[:body])
   end
