4b0ee0a1c45daac6528b41b42f68f98ec4968dad
Ted <ekeast328@gmail.com>
2016-04-19 12:17:02 -0400

    trying again

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,7 +3,7 @@ class IncomingController < ApplicationController
 
   def create
     @user = User.where(email: "#{params[:sender]}")
-    @topic = @user.topic.find_or_create_by(title: params[:subject])
+    @topic = @user.topics.find_or_create_by(title: params[:subject])
     @bookmark = @topic.bookmarks.new(url: params[:body])
   end
 end
